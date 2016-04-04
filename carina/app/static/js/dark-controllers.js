ed5bc17929cd78d540409c3a432be88b4d87083c
sls498 <shanell.smith@utexas.edu>
2016-04-04 01:38:27 -0500

    Implemented angular table for Cards. Added cost images from icon map

--- carina/app/static/js/dark-controllers.js
+++ carina/app/static/js/dark-controllers.js
@@ -9,7 +9,25 @@ dark.controller('HomeController', ['$scope', function($scope, Home) {
 } ]);
 
 dark.controller('CardsController', ['$scope','Cards', function($scope, Cards) {
-	$scope.cards = Cards.query();
+	$scope.sortType     = 'Card'; // set the default sort type
+	$scope.sortReverse  = false;  // set the default sort order
+	
+	$scope.range = function(start, end)
+	{
+	    var array = new Array();
+	    for(var i = start; i < end; i++)
+	    {
+	        array.push(i);
+	    }
+	    return array;
+	}
+	
+	// create the list of entries
+	$scope.cards = [
+	                { artist: "Steve Argyle", card: "Admonition Angel", set: "WorldWake",  type: "Creature - Angel", cmc: 6, cost: [{type:"any", value:6}, {type:"white", value:4}, {type:"blue", value:0}, {type:"green", value:0}, {type:"red", value:0}, {type:"black", value:0}]},
+	                { artist: "Jeff A. Manges", card: "Deep Water", set: "The Dark",  type: "Enchantment", cmc: 0, cost: [{type:"any", value:6}, {type:"white", value:2}, {type:"blue", value:0}, {type:"green", value:0}, {type:"red", value:0}, {type:"black", value:0}]},
+	                { artist: "Nils Hamm", card: "Mountain", set: "All Sets",  type: "Basic Land", cmc: 0, cost: [{type:"any", value:6}, {type:"white", value:1}, {type:"blue", value:0}, {type:"green", value:0}, {type:"red", value:0}, {type:"black", value:0}]}
+	  ];
 } ]);
 
 dark.controller('SetsController', ['$scope', function($scope, Sets) {
@@ -21,10 +39,20 @@ dark.controller('ArtistsController', ['$scope', function($scope, Artists) {
 	$scope.sortType     = 'Artist'; // set the default sort type
 	$scope.sortReverse  = false;  // set the default sort order
 	
+	$scope.range = function(start, end)
+	{
+	    var array = new Array();
+	    for(var i = start; i < end; i++)
+	    {
+	        array.push(i);
+	    }
+	    return array;
+	}
+	
 	// create the list of entries
 	$scope.artists = [
-	                { artist: "Steve Argyle", card: "Admonition Angel", set: "WorldWake",  type: "Creature - Angel", cmc: 6, cost: 6},
-	                { artist: "Jeff A. Manges", card: "Deep Water", set: "The Dark",  type: "Enchantment", cmc: 0, cost: 0},
-	                { artist: "Nils Hamm", card: "Mountain", set: "All Sets",  type: "Basic Land", cmc: 0, cost: 0}
+	                { artist: "Steve Argyle", card: "Admonition Angel", set: "WorldWake",  type: "Creature - Angel", cmc: 6, cost: [{type:"any", value:6}, {type:"white", value:4}, {type:"blue", value:0}, {type:"green", value:0}, {type:"red", value:0}, {type:"black", value:0}]},
+	                { artist: "Jeff A. Manges", card: "Deep Water", set: "The Dark",  type: "Enchantment", cmc: 0, cost: [{type:"any", value:6}, {type:"white", value:2}, {type:"blue", value:0}, {type:"green", value:0}, {type:"red", value:0}, {type:"black", value:0}]},
+	                { artist: "Nils Hamm", card: "Mountain", set: "All Sets",  type: "Basic Land", cmc: 0, cost: [{type:"any", value:6}, {type:"white", value:1}, {type:"blue", value:0}, {type:"green", value:0}, {type:"red", value:0}, {type:"black", value:0}]}
 	  ];
 } ]);
