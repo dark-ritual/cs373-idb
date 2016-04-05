11a46ddfadf2073b0594ae90ede4b03cf37f93be
sls498 <shanell.smith@utexas.edu>
2016-04-04 22:53:52 -0500

    Added text wrap to table

--- carina/app/static/js/dark-controllers.js
+++ carina/app/static/js/dark-controllers.js
@@ -2,6 +2,12 @@
 
 /* Controllers */
 
+var testData = [
+                { artist: "Steve Argyle Steve Argyle Steve Argyle Steve Argyle Steve Argyle", card: "Admonition Angel Admonition Angel Admonition Angel Admonition Angel", set: "WorldWake WorldWake WorldWake WorldWake WorldWake WorldWake WorldWake WorldWake WorldWake",  type: "Creature - Angel Creature - Angel Creature - Angel Creature - Angel Creature - Angel Creature - Angel Creature - Angel Creature - Angel", cmc: 666666666666666666666666666666666666666666666666666666666666, cost: [{type:"any6", value:6}, {type:"white", value:6}, {type:"blue", value:6}, {type:"chaos", value:1}, {type:"hundred", value:1}, {type:"slash", value:1}]},
+                { artist: "Jeff A. Manges", card: "Deep Water", set: "The Dark",  type: "Enchantment", cmc: 0, cost: [{type:"any9", value:1}, {type:"anyX", value:1}, {type:"untap", value:1}, {type:"snow", value:1}, {type:"green-blue", value:1}, {type:"any2-green", value:1}]},
+                { artist: "Nils Hamm", card: "Mountain", set: "All Sets",  type: "Basic Land", cmc: 0, cost: [{type:"any12", value:1}, {type:"white-black", value:1}, {type:"tapA", value:1}, {type:"infinity", value:1}, {type:"million", value:1}, {type:"phyrexia", value:1}]}
+  ];
+
 var darkControllers = angular.module('darkControllers', []);
 
 dark.controller('HomeController', ['$scope', function($scope, Home) {
@@ -23,11 +29,7 @@ dark.controller('CardsController', ['$scope','Cards', function($scope, Cards) {
 	}
 	
 	// create the list of entries
-	$scope.cards = [
-	                { artist: "Steve Argyle", card: "Admonition Angel", set: "WorldWake",  type: "Creature - Angel", cmc: 6, cost: [{type:"any6", value:1}, {type:"white", value:1}, {type:"blue", value:1}, {type:"chaos", value:1}, {type:"hundred", value:1}, {type:"slash", value:1}]},
-	                { artist: "Jeff A. Manges", card: "Deep Water", set: "The Dark",  type: "Enchantment", cmc: 0, cost: [{type:"any9", value:1}, {type:"anyX", value:1}, {type:"untap", value:1}, {type:"snow", value:1}, {type:"green-blue", value:1}, {type:"any2-green", value:1}]},
-	                { artist: "Nils Hamm", card: "Mountain", set: "All Sets",  type: "Basic Land", cmc: 0, cost: [{type:"any12", value:1}, {type:"white-black", value:1}, {type:"tapA", value:1}, {type:"infinity", value:1}, {type:"million", value:1}, {type:"phyrexia", value:1}]}
-	  ];
+	$scope.cards = testData;
 } ]);
 
 dark.controller('SetsController', ['$scope', function($scope, Sets) {
@@ -45,12 +47,7 @@ dark.controller('SetsController', ['$scope', function($scope, Sets) {
 	}
 	
 	// create the list of entries
-	$scope.sets = [
-	                { artist: "Steve Argyle", card: "Admonition Angel", set: "WorldWake",  type: "Creature - Angel", cmc: 6, cost: [{type:"any", value:6}, {type:"white", value:4}, {type:"blue", value:0}, {type:"green", value:0}, {type:"red", value:0}, {type:"black", value:0}]},
-	                { artist: "Jeff A. Manges", card: "Deep Water", set: "The Dark",  type: "Enchantment", cmc: 0, cost: [{type:"any", value:6}, {type:"white", value:2}, {type:"blue", value:0}, {type:"green", value:0}, {type:"red", value:0}, {type:"black", value:0}]},
-	                { artist: "Nils Hamm", card: "Mountain", set: "All Sets",  type: "Basic Land", cmc: 0, cost: [{type:"any", value:6}, {type:"white", value:1}, {type:"blue", value:0}, {type:"green", value:0}, {type:"red", value:0}, {type:"black", value:0}]}
-	  ];
-
+	$scope.sets = testData;
 } ]);
 
 dark.controller('ArtistsController', ['$scope', function($scope, Artists) {
@@ -68,9 +65,5 @@ dark.controller('ArtistsController', ['$scope', function($scope, Artists) {
 	}
 	
 	// create the list of entries
-	$scope.artists = [
-	                { artist: "Steve Argyle", card: "Admonition Angel", set: "WorldWake",  type: "Creature - Angel", cmc: 6, cost: [{type:"any", value:6}, {type:"white", value:4}, {type:"blue", value:0}, {type:"green", value:0}, {type:"red", value:0}, {type:"black", value:0}]},
-	                { artist: "Jeff A. Manges", card: "Deep Water", set: "The Dark",  type: "Enchantment", cmc: 0, cost: [{type:"any", value:6}, {type:"white", value:2}, {type:"blue", value:0}, {type:"green", value:0}, {type:"red", value:0}, {type:"black", value:0}]},
-	                { artist: "Nils Hamm", card: "Mountain", set: "All Sets",  type: "Basic Land", cmc: 0, cost: [{type:"any", value:6}, {type:"white", value:1}, {type:"blue", value:0}, {type:"green", value:0}, {type:"red", value:0}, {type:"black", value:0}]}
-	  ];
+	$scope.artists = testData;
 } ]);
