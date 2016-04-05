20473205533a2ffbae271c3b6d77dbbe48646e6d
sls498 <shanell.smith@utexas.edu>
2016-04-04 21:48:25 -0500

    Finished mapping icons. Testing icon display on Cards.html

--- carina/app/static/js/dark-controllers.js
+++ carina/app/static/js/dark-controllers.js
@@ -24,9 +24,9 @@ dark.controller('CardsController', ['$scope','Cards', function($scope, Cards) {
 	
 	// create the list of entries
 	$scope.cards = [
-	                { artist: "Steve Argyle", card: "Admonition Angel", set: "WorldWake",  type: "Creature - Angel", cmc: 6, cost: [{type:"any", value:6}, {type:"white", value:4}, {type:"blue", value:0}, {type:"green", value:0}, {type:"red", value:0}, {type:"black", value:0}]},
-	                { artist: "Jeff A. Manges", card: "Deep Water", set: "The Dark",  type: "Enchantment", cmc: 0, cost: [{type:"any", value:6}, {type:"white", value:2}, {type:"blue", value:0}, {type:"green", value:0}, {type:"red", value:0}, {type:"black", value:0}]},
-	                { artist: "Nils Hamm", card: "Mountain", set: "All Sets",  type: "Basic Land", cmc: 0, cost: [{type:"any", value:6}, {type:"white", value:1}, {type:"blue", value:0}, {type:"green", value:0}, {type:"red", value:0}, {type:"black", value:0}]}
+	                { artist: "Steve Argyle", card: "Admonition Angel", set: "WorldWake",  type: "Creature - Angel", cmc: 6, cost: [{type:"any6", value:1}, {type:"white", value:1}, {type:"blue", value:1}, {type:"green", value:1}, {type:"red", value:1}, {type:"black", value:1}]},
+	                { artist: "Jeff A. Manges", card: "Deep Water", set: "The Dark",  type: "Enchantment", cmc: 0, cost: [{type:"any9", value:1}, {type:"anyX", value:1}, {type:"untap", value:1}, {type:"snow", value:1}, {type:"green-blue", value:1}, {type:"any2-green", value:1}]},
+	                { artist: "Nils Hamm", card: "Mountain", set: "All Sets",  type: "Basic Land", cmc: 0, cost: [{type:"any12", value:1}, {type:"white-black", value:1}, {type:"tapA", value:1}, {type:"infinity", value:1}, {type:"half", value:1}, {type:"phyrexia-blue", value:1}]}
 	  ];
 } ]);
 
