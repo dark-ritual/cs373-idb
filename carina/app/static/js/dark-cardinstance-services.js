264adbd15f5a05d6d891c97054c534844790de44
Javier E. De la Garza S <javier.delagarza@gmail.com>
2016-04-06 17:46:10 -0500

    adding card instance angular page and temp nav button

---
+++ carina/app/static/js/dark-cardinstance-services.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/* Card Instance Services */
+
+var instanceServices = angular.module('cardinstanceServices', ['ngResource']);
+
+cardinstanceServices.factory('CardInstance', [ '$resource', function($resource) {
+/*	
+	return $resource('../api/artists', {}, {
+		query: {
+			method: 'GET',
+			isArray: true
+		}
+	});
+	*/
+}]);
